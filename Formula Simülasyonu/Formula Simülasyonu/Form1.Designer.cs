61991dd28b2d5d3feef670bf2d93f8a56a8cb13c
Bora8 <boraulker8@gmail.com>
2024-03-05 02:28:29 +0300

    Formula Simulasyonu
    SQL veritabanı ve C# ile oluşturulmuş F1 sezon simülasyonu

---
+++ Formula Simülasyonu/Formula Simülasyonu/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace Formula_Simülasyonu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.listView3 = new System.Windows.Forms.ListView();
+            this.columnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader11 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader12 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(720, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Sıradaki Yarış";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listView1);
+            this.groupBox1.Location = new System.Drawing.Point(27, 56);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(260, 485);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Son Yarış";
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader5,
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(3, 18);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(254, 464);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Pilot";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Takım";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Puan";
+            this.columnHeader3.Width = 30;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Sıra";
+            this.columnHeader5.Width = 20;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(117, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listView2);
+            this.groupBox2.Location = new System.Drawing.Point(315, 56);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(260, 485);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Sezon Sıralaması";
+            // 
+            // listView2
+            // 
+            this.listView2.BackColor = System.Drawing.SystemColors.Control;
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader4,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8});
+            this.listView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(3, 18);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(254, 464);
+            this.listView2.TabIndex = 0;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Sıra";
+            this.columnHeader4.Width = 20;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Pilot";
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Takım";
+            this.columnHeader7.Width = 62;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "Puan";
+            this.columnHeader8.Width = 30;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.listView3);
+            this.groupBox3.Location = new System.Drawing.Point(594, 56);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(260, 485);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Takımlar";
+            // 
+            // listView3
+            // 
+            this.listView3.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.listView3.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader9,
+            this.columnHeader11,
+            this.columnHeader12});
+            this.listView3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView3.HideSelection = false;
+            this.listView3.Location = new System.Drawing.Point(3, 18);
+            this.listView3.Name = "listView3";
+            this.listView3.Size = new System.Drawing.Size(254, 464);
+            this.listView3.TabIndex = 0;
+            this.listView3.UseCompatibleStateImageBehavior = false;
+            this.listView3.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader9
+            // 
+            this.columnHeader9.Text = "Sıra";
+            this.columnHeader9.Width = 20;
+            // 
+            // columnHeader11
+            // 
+            this.columnHeader11.Text = "Takım";
+            this.columnHeader11.Width = 62;
+            // 
+            // columnHeader12
+            // 
+            this.columnHeader12.Text = "Puan";
+            this.columnHeader12.Width = 30;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(41, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClientSize = new System.Drawing.Size(879, 553);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ListView listView3;
+        private System.Windows.Forms.ColumnHeader columnHeader9;
+        private System.Windows.Forms.ColumnHeader columnHeader11;
+        private System.Windows.Forms.ColumnHeader columnHeader12;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Formula Simülasyonu/Formula Simülasyonu/Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Formula_Simülasyonu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection("Data Source=KOELSYS\\SQLEXPRESS;Initial Catalog=FormulaSimülasyonu;Integrated Security=True");
+        int sayi = 0;
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            
+            
+            sayi++;
+            label2.Text = sayi.ToString();
+            switch (sayi)
+            {
+                case 0:
+                    label1.Text = "Melbourne - Avustralya Grand Prix";
+                    break;
+                case 1:
+                    label1.Text = "Imola - Emilia Romagna Grand Prix";
+                    break;
+                case 2:
+                    label1.Text = "Portimao - Portekiz Grand Prix";
+                    break;
+                case 3:
+                    label1.Text = "Circuit de Barcelona-Catalunya - İspanya Grand Prix";
+                    break;
+                case 4:
+                    label1.Text = "Monaco - Monaco Grand Prix";
+                    break;
+                case 5:
+                    label1.Text = "Baku City Circuit - Azerbaycan Grand Prix";
+                    break;
+                case 6:
+                    label1.Text = "Circuit Paul Ricard - Fransa Grand Prix";
+                    break;
+                case 7:
+                    label1.Text = "Red Bull Ring - Avusturya Grand Prix";
+                    break;
+                case 8:
+                    label1.Text = "Silverstone Circuit - İngiltere Grand Prix";
+                    break;
+                case 9:
+                    label1.Text = "Hungaroring - Macaristan Grand Prix";
+                    break;
+                case 10:
+                    label1.Text = "Spa-Francorchamps - Belçika Grand Prix";
+                    break;
+                case 11:
+                    label1.Text = "Zandvoort - Hollanda Grand Prix";
+                    break;
+                case 12:
+                    label1.Text = "Monza - İtalya Grand Prix";
+                    break;
+                case 13:
+                    label1.Text = "Sochi Autodrom - Rusya Grand Prix";
+                    break;
+                case 14:
+                    label1.Text = "Marina Bay Street Circuit - Singapur Grand Prix";
+                    break;
+                case 15:
+                    label1.Text = "Suzuka Circuit - Japonya Grand Prix";
+                    break;
+                case 16:
+                    label1.Text = "Circuit of the Americas - Amerika Birleşik Devletleri Grand Prix";
+                    break;
+                case 17:
+                    label1.Text = "Autódromo Hermanos Rodríguez - Meksika Grand Prix";
+                    break;
+                case 18:
+                    label1.Text = "Interlagos - Brezilya Grand Prix";
+                    break;
+                case 19:
+                    label1.Text = "Albert Park Circuit - Avustralya Grand Prix";
+                    break;
+                case 20:
+                    label1.Text = "Jeddah Street Circuit - Suudi Arabistan Grand Prix";
+                    break;
+                case 21:
+                    label1.Text = "Yas Marina Circuit - Abu Dabi Grand Prix";
+                    break;
+                case 22:
+                    label1.Text = "Yas Marina Circuit - Abu Dabi Grand Prix";
+                    break;
+                case 23:
+                    label1.Text = "Shanghai International Circuit - Çin Grand Prix";
+                    break;
+                default:
+                    label1.Text = "Bilinmeyen Pist";
+                    break;
+            }       
+            grid();
+            sirala();
+            sezonsilamasi();
+            if(sayi == 23)
+            {
+                MessageBox.Show("Sezon Bitti. Uygulama Kapanacak.");
+                Application.Exit();
+                
+            }
+            
+        }
+        private void grid()
+        {
+            object[,] dizi = new object[2, 20];
+            
+            Random random = new Random();
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("SELECT * FROM PilotlarSiralamasi", baglanti);
+            SqlDataReader dr = komut.ExecuteReader();
+
+            int i = 0;
+            while (dr.Read())
+            {
+                dizi[0, i] = dr["Pilotlar"].ToString();
+                dizi[1, i] = dr["Guc"];
+                i++;
+            }
+            dr.Close();
+
+            for (int j = 0; j < 20; j++)
+            {
+                string pilotAdi = dizi[0, j].ToString();
+                int gucu = Convert.ToInt32(dizi[1, j]);
+                int yeniPuan = gucu + random.Next(-10, 11);
+
+                SqlCommand komut1 = new SqlCommand("UPDATE PilotlarSiralamasi SET SonYaris = @yeniPuan WHERE Pilotlar = @pilotAdi", baglanti);
+                komut1.Parameters.AddWithValue("@yeniPuan", yeniPuan);
+                komut1.Parameters.AddWithValue("@pilotAdi", pilotAdi);
+                
+                komut1.ExecuteNonQuery();
+
+                
+            }
+
+            baglanti.Close();
+        }
+        private void sirala()
+        {
+            baglanti.Open();
+            object[,] dizi1 = new object[3, 20];
+
+            SqlCommand komut = new SqlCommand("SELECT * FROM PilotlarSiralamasi ORDER BY SonYaris DESC, NEWID()", baglanti);
+            SqlDataReader dr = komut.ExecuteReader();
+
+            int a = 0; int p = 0;
+            while (dr.Read())
+            {
+                switch (a)
+                {
+                    case (0):
+                        p = 25;
+                        break;
+                    case (1):
+                        p = 18;
+                        break;
+                    case (2):
+                        p = 15;
+                        break;
+                    case (3):
+                        p = 12;
+                        break;
+                    case (4):
+                        p = 10;
+                        break;
+                    case (5):
+                        p = 8;
+                        break;
+                    case (6):
+                        p = 6;
+                        break;
+                    case (7):
+                        p = 4;
+                        break;
+                    case (8):
+                        p = 2;
+                        break;
+                    case (9):
+                        p = 1;
+                        break;
+                    default: p = 0; break;
+                }
+                
+
+                string[] row = { (a + 1).ToString(), dr["Pilotlar"].ToString(), dr["Takımlar"].ToString(), p.ToString() };
+                var item = new ListViewItem(row);
+                listView1.Items.Add(item);
+                
+                dizi1[0,a] = dr["Pilotlar"].ToString();
+                dizi1[1, a] = p;
+                dizi1[2, a] = dr["Takımlar"];
+                
+                a++;
+            }
+            dr.Close();
+            for (int i = 0; i < 20; i++)
+            {
+                SqlCommand pkomut = new SqlCommand("UPDATE SezonSiralamasiPilotlar SET Puan = Puan + @puan WHERE Pilotlar = @pilotlar", baglanti);
+                pkomut.Parameters.AddWithValue("@puan", dizi1[1, i]);
+                pkomut.Parameters.AddWithValue("@pilotlar", dizi1[0, i]);
+                pkomut.ExecuteNonQuery();
+            }
+            for (int i = 0; i < 20; i++)
+            {
+                SqlCommand pkomut = new SqlCommand("UPDATE SezonSiralamasiTakımlar SET Puan = Puan + @puan WHERE Takımlar = @takimlar", baglanti);
+                pkomut.Parameters.AddWithValue("@puan", dizi1[1, i]);
+                pkomut.Parameters.AddWithValue("@takimlar", dizi1[2, i]);
+                pkomut.ExecuteNonQuery();
+            }
+
+            baglanti.Close();
+            
+        }
+        private void sezonsilamasi()
+        {
+            listView2.Items.Clear();
+            listView3.Items.Clear();
+
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("SELECT * FROM SezonSiralamasiPilotlar ORDER BY Puan DESC, NEWID()", baglanti);
+            SqlDataReader dr = komut.ExecuteReader();
+            int a = 0;
+            while (dr.Read())
+            {
+                a++;
+                string[] row = { a.ToString(),dr["Pilotlar"].ToString(), dr["Takımlar"].ToString(), dr["Puan"].ToString() };
+                var item = new ListViewItem(row);
+                listView2.Items.Add(item);
+            }
+
+            dr.Close();
+
+            SqlCommand komut1 = new SqlCommand("SELECT * FROM SezonSiralamasiTakımlar ORDER BY Puan DESC, NEWID()", baglanti);
+            SqlDataReader dr1 = komut1.ExecuteReader();
+            int b = 0;
+            while (dr1.Read())
+            {
+                b++;
+                string[] row1 = { b.ToString(), dr1["Takımlar"].ToString(), dr1["Puan"].ToString() };
+                var item1 = new ListViewItem(row1);
+                listView3.Items.Add(item1);
+            }
+            baglanti.Close();
+        }
+
+        private void ResetTablolar()
+        {
+            try
+            {
+                baglanti.Open();
+
+                
+                SqlCommand resetKomut1 = new SqlCommand("UPDATE PilotlarSiralamasi SET SonYaris = 0", baglanti);
+                resetKomut1.ExecuteNonQuery();
+
+                
+                SqlCommand resetKomut2 = new SqlCommand("UPDATE SezonSiralamasiPilotlar SET Puan = 0", baglanti);
+                resetKomut2.ExecuteNonQuery();
+
+                
+                SqlCommand resetKomut3 = new SqlCommand("UPDATE SezonSiralamasiTakımlar SET Puan = 0", baglanti);
+                resetKomut3.ExecuteNonQuery();
+
+                baglanti.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Tablolar sıfırlanırken bir hata oluştu: " + ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ResetTablolar();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    
+}
